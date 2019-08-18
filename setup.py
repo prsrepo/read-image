8ed73467be49abfb1c638dea584c14f9f42d4880
Purushotham Reddy <kspreddy94@gmail.com>
2019-08-18 15:26:53 +0530

    #updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
-from pip.req import parse_requirements
+import re, uuid
+from setuptools import setup, find_packages
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 install_reqs = parse_requirements('requirements.txt')
-
-reqs = [str(ir.req) for ir in install_reqs]
+reqs = install_reqs
 
 setup(
    name='read-image',
@@ -12,5 +17,16 @@ setup(
    author='pcbuddies',
    author_email='pcbuddies',
    packages=['read_image'], 
-   install_requires=reqs
+   install_requires=reqs,
+   keywords="Python image reader",
+   classifiers=[
+         'Development Status :: 4 - Beta',
+         'Topic :: Software Development :: Libraries',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent',
+         'Programming Language :: Python',
+         'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3.7',
+   ],
+   zip_safe=True
 )
